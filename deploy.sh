f7ea2f2962021d0f24dc61fd250091d54f2d8931
nsankethreddy <sankethn1947@gmail.com>
2020-07-05 19:58:16 +0530

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 docker build -t skete/multi-client:latest -t skete/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t skete/multi-server:latest -t skete/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t skete/multi-worker:latest -t skete/multi-worker:$SHA -f ./worker /Dockerfile ./worker
+docker build -t skete/multi-worker:latest -t skete/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push skete/multi-client:latest
 docker push skete/multi-server:latest
 docker push skete/multi-worker:latest
@@ -8,6 +9,7 @@ docker push skete/multi-worker:latest
 docker push skete/multi-client:$SHA
 docker push skete/multi-server:$SHA
 docker push skete/multi-worker:$SHA
+
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=skete/multi-server:$SHA
 kubectl set image deployments/client-deployment client=skete/multi-client:$SHA
